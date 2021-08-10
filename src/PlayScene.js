5d6a72bab743d2a963eaddc959b4c5175ef8f562
Jacob Hornung <hornunjb@mail.uc.edu>
2021-08-09 21:13:01 -0400

    running animations

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -19,7 +19,35 @@ class PlayScene extends Phaser.Scene {
     //strength the dinosaur will be pulled to the ground at 5000 pixels per second
     .setGravityY(5000);
 
+    this.initAnims();
     this.handleInputs();
+  }
+
+  initAnims() {
+    //uses the dino-run png using the last two frames to create the running animation
+    this.anims.create({
+      key: 'dino-run',
+      //frame starts at index zero, that's why we have the start and end values at 2 and 3
+      frames: this.anims.generateFrameNumbers('dino', {start: 2, end: 3}),
+      frameRate: 10,
+      repeat: -1,
+    })
+
+    this.anims.create({
+      //dinosaur is ducking
+      key: 'dino-down-anim',
+      frames: this.anims.generateFrameNumbers('dino-down', {start: 0, end: 1}),
+      frameRate: 10,
+      repeat: -1,
+    })
+
+    this.anims.create({
+      //enemy bird
+      key: 'enemy-dino-fly',
+      frames: this.anims.generateFrameNumbers('enemy-bird', {start: 0, end: 1}),
+      frameRate: 6,
+      repeat: -1,
+    })
   }
 
   handleInputs() {
@@ -36,6 +64,16 @@ class PlayScene extends Phaser.Scene {
   update() {
     //every update the ground will move gameSpeed pixels
     this.ground.tilePositionX += this.gameSpeed;
+
+    //if the dinosaur is chaning position, then we are jumping so we stop the animations
+    if(this.dino.body.deltaAbsY() > 0) {
+      this.dino.anims.stop();
+      this.dino.setTexture('dino');
+    }
+    else {
+      this.dino.play('dino-run', true);
+    }
+
   }
 
 }

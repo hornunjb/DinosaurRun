6528e55a4bc9aa489a4d4d95161c132cb8e817de
Jacob Hornung <hornunjb@mail.uc.edu>
2021-08-09 23:23:01 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,40 +1,39 @@
-
 import Phaser from 'phaser';
 
 import PlayScene from './PlayScene';
 import PreloadScene from './PreloadScene';
 
 const config = {
-  type: Phaser.AUTO,
-  width: 1000,
-  height: 340,
-  pixelArt: true,
-  transparent: true,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      debug: false
-    }
-  },
-  scene: [PreloadScene, PlayScene]
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 340,
+    pixelArt: true,
+    transparent: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scene: [PreloadScene, PlayScene]
 };
 
 new Phaser.Game(config);
 
 // USING GEOLOCATION API
+
 var x = document.getElementById("coord");
 function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
-  weatherBallon(position.coords.latitude, position.coords.longitude);
+    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+    weatherBallon(position.coords.latitude, position.coords.longitude);
 }
 
 
@@ -42,34 +41,32 @@ function showPosition(position) {
 
 const key = '2513864aca32eeb68b96cb812043b641';
 
-function weatherBallon( lat, lon ) {
-	fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key)  
-	.then(function(resp) { return resp.json() })
-	.then(function(data) {
-		drawWeather(data);
-	})
-	.catch(function() {
-	});
+function weatherBallon(lat, lon) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key).then(function (resp) {
+        return resp.json()
+    }).then(function (data) {
+        drawWeather(data);
+    }).catch(function () {});
 }
-function drawWeather( d ) {
-  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
-  var description = d.weather[0].description; 
-	
-	document.getElementById('description').innerHTML = description;
-	document.getElementById('temp').innerHTML = celcius + '&deg;';
-	document.getElementById('location').innerHTML = d.name;
-  
-  if( description.indexOf('rain') > 0 ) {
-  	document.body.className = 'rainy';
-  } else if( description.indexOf('cloud') > 0 ) {
-  	document.body.className = 'cloudy';
-  } else if( description.indexOf('sunny') > 0 ) {
-  	document.body.className = 'sunny';
-  } else {
-  	document.body.className = 'clear';
-  }
+function drawWeather(d) {
+    var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+    var fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
+    var description = d.weather[0].description;
+
+    document.getElementById('description').innerHTML = description;
+    document.getElementById('temp').innerHTML = celcius + '&deg;';
+    document.getElementById('location').innerHTML = d.name;
+
+    if (description.indexOf('rain') > 0) {
+        document.body.className = 'rainy';
+    } else if (description.indexOf('cloud') > 0) {
+        document.body.className = 'cloudy';
+    } else if (description.indexOf('sunny') > 0) {
+        document.body.className = 'sunny';
+    } else {
+        document.body.className = 'clear';
+    }
 }
-window.onload = function() {
-  getLocation();
+window.onload = function () {
+    getLocation();
 }

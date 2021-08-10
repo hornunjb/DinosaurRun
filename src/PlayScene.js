6b47958c8b8414949189b8c48b29ef947b8114c4
Jacob Hornung <hornunjb@mail.uc.edu>
2021-08-09 21:01:27 -0400

    dinosaur jumping controls

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -10,10 +10,25 @@ class PlayScene extends Phaser.Scene {
     this.gameSpeed = 10;
     const {height, width} = this.game.config;
 
-    //adds scrollable infinite structure which we use as ground
+    //scrollable infinite structure which we use as ground
     this.ground = this.add.tileSprite(0, height, width, 26, 'ground').setOrigin(0, 1)
-    //adds dinosaur
-    this.dino = this.physics.add.sprite(0, height, 'dino-idle').setOrigin(0, 1);
+    //dinosaur
+    this.dino = this.physics.add.sprite(0, height, 'dino-idle')
+    .setOrigin(0, 1)
+    .setCollideWorldBounds(true)
+    //strength the dinosaur will be pulled to the ground at 5000 pixels per second
+    .setGravityY(5000);
+
+    this.handleInputs();
+  }
+
+  handleInputs() {
+    //jumping
+    this.input.keyboard.on('keydown_SPACE', () => {
+      //prevents dinosaur from jumping mid-air
+      if(!this.dino.body.onFloor()) {return;}
+      this.dino.setVelocityY(-1600);
+    })
   }
 
 

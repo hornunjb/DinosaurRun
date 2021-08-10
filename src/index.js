29a9ff0d407797f4a4a625e74d9cf6bcb54bb52a
Jacob Hornung <hornunjb@mail.uc.edu>
2021-08-09 23:21:03 -0400

    geolocation api and weather api

--- src/index.js
+++ src/index.js
@@ -20,3 +20,56 @@ const config = {
 };
 
 new Phaser.Game(config);
+
+// USING GEOLOCATION API
+var x = document.getElementById("coord");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+  weatherBallon(position.coords.latitude, position.coords.longitude);
+}
+
+
+// USING WEATHER API
+
+const key = '2513864aca32eeb68b96cb812043b641';
+
+function weatherBallon( lat, lon ) {
+	fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key)  
+	.then(function(resp) { return resp.json() })
+	.then(function(data) {
+		drawWeather(data);
+	})
+	.catch(function() {
+	});
+}
+function drawWeather( d ) {
+  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+  var description = d.weather[0].description; 
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+  
+  if( description.indexOf('rain') > 0 ) {
+  	document.body.className = 'rainy';
+  } else if( description.indexOf('cloud') > 0 ) {
+  	document.body.className = 'cloudy';
+  } else if( description.indexOf('sunny') > 0 ) {
+  	document.body.className = 'sunny';
+  } else {
+  	document.body.className = 'clear';
+  }
+}
+window.onload = function() {
+  getLocation();
+}
